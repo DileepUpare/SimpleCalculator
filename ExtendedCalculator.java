f6a1e2cedc778d6c1b314d6a1c0c7705bbcaa900
01fe22bca119 <157907429+DileepUpare@users.noreply.github.com>
2024-02-19 15:17:29 +0530

    Add files via upload

---
+++ ExtendedCalculator.java
@@ -0,0 +1,9 @@
+public class ExtendedCalculator extends Calculator {
+    public int square(int num) {
+        return num * num;
+    }
+
+    public int cube(int num) {
+        return num * num * num;
+    }
+}
